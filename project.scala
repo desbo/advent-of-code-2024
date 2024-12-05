3dbff4e3e3d2eda918514e4bbca13e88e9b57f8f
Sam Desborough <sam.desborough@gmail.com>
2024-12-05 17:50:50 +0000

    add test

--- project.scala
+++ project.scala
@@ -5,3 +5,4 @@
 //> using dep "com.lihaoyi::os-lib::0.11.3"
 //> using dep "org.typelevel::cats-effect::3.5.7"
 //> using dep "com.softwaremill.sttp.client3::core:3.10.1"
+//> using test.dep org.scalameta::munit::0.7.29
