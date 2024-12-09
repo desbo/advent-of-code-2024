ce2283afc56830231021472edef6beca029deca1
Sam Desborough <sam.desborough@jpmorgan.com>
2024-12-09 18:22:13 +0000

    'wip'

--- src/main/scala/aoc/twentyfour/Day9.scala
+++ src/main/scala/aoc/twentyfour/Day9.scala
@@ -1,52 +1,121 @@
 package aoc
 package twentyfour
 
-import aoc.twentyfour.Day9.blocks
+import aoc.twentyfour.Day9.Block
 import cats.syntax.all.*
 
 import scala.annotation.tailrec
 
-object Day9 extends Solution[String, BigInt]:
-  override def parse(input: String): String = input
+object Day9 extends Solution[List[Block], BigInt]:
+  case class Block(size: Int, id: Int, empty: Boolean):
+    def remove(space: Int): Option[Block] =
+      if space >= size then none else copy(size = size - space).some
 
-  def blocks(input: String): String =
-    input
-      .dropRight(1)
-      .zipWithIndex
-      .flatMap:
-        case (char, i) if i % 2 == 0 =>
-          List.fill(char.toString.toInt)((i / 2).toString)
-        case (char, i) =>
-          List.fill(char.toString.toInt)(".")
-      .mkString
+    def add(space: Int): Block = copy(size = size + space)
 
-  def defrag(blocks: String): String =
+  def take(blocks: List[Block], size: Int): List[Block] =
+    def go(sizeLeft: Int, taken: List[Block], blocks: List[Block]): List[Block] =
+      if sizeLeft <= 0 then taken
+      else
+        blocks match
+          case b :: t =>
+            b.remove(sizeLeft)
+              .fold(go(sizeLeft - b.size, taken :+ b, t)): bigBlock =>
+                go(
+                  sizeLeft - bigBlock.size,
+                  taken :+ bigBlock.copy(size = sizeLeft),
+                  bigBlock.copy(size = sizeLeft) +: t
+                )
+          case Nil => taken
+
+    go(size, List.empty, blocks)
+
+  def drop(blocks: List[Block], size: Int): List[Block] =
+    def go(sizeLeft: Int, dropped: List[Block]): List[Block] =
+      if sizeLeft <= 0 then dropped
+      else
+        dropped match
+          case b :: t =>
+            b.remove(sizeLeft)
+              .fold(go(sizeLeft - b.size, t)): bigBlock =>
+                go(sizeLeft - b.size, bigBlock +: t)
+          case Nil => dropped
+
+    go(size, blocks)
+
+  def pack(blocks: List[Block]): List[Block] =
+    val (packed, lastOne) = blocks.foldLeft((List.empty[Block], none[Block])):
+      case ((packed, None), block) =>
+        (packed, block.some)
+      case ((packed, Some(current)), block) if current.id == block.id =>
+        (packed, current.add(block.size).some)
+      case ((packed, Some(current)), block) =>
+        (packed :+ current, block.some)
+
+    lastOne.fold(packed)(packed.appended)
+
+  override def parse(input: String): List[Block] = input
+    .takeWhile(c => c >= 48 && c <= 57)
+    .zipWithIndex
+    .toList
+    .map:
+      case (char, i) if i % 2 == 0 =>
+        Block(size = char.toString.toInt, id = i / 2, empty = false)
+      case (char, i) =>
+        Block(size = char.toString.toInt, id = -999, empty = true)
+
+  def defrag(blocks: List[Block]): List[Block] =
     @tailrec
-    def process(defragged: String, toProcess: List[Char], blocks: List[Char]): String =
-      toProcess match
-        case b :: remaining =>
-          blocks match
-            case '.' :: rest => process(s"$defragged$b", remaining, rest)
-            case c :: rest   => process(s"$defragged$c", b +: remaining, rest)
-        case Nil =>
-          blocks match
-            case '.' :: _ => defragged
-            case c :: _   => defragged.concat(c.toString)
+    def process(defragged: List[Block], filesToDefrag: List[Block], disk: List[Block]): List[Block] =
+      println("=====================")
+      println(s"defragged: ${render(defragged)}")
+      println(s"files left: ${render(filesToDefrag)}, $filesToDefrag")
+      println(s"disk: ${render(disk)}")
 
-    val freeSpace = blocks.count(_ == '.')
+      filesToDefrag match
+        case Nil => defragged
+        case _ =>
+          disk match
+            case e :: rest if e.empty =>
+              println(s"adding ${take(filesToDefrag, e.size)} to empty spot")
+              process(defragged ++ take(filesToDefrag, e.size), drop(filesToDefrag, e.size), rest)
+            case num :: rest =>
+              println(s"copying $num to defragged")
+              process(defragged :+ num, filesToDefrag, rest)
+            case Nil => defragged
 
-    process("", blocks.reverse.take(freeSpace).filterNot(_ == '.').toList, blocks.toList)
-      .padTo(blocks.length, '.')
+    val freeSpace = blocks.filter(_.empty).map(_.size).sum
 
-  def checksum(blocks: String): BigInt =
+    println(blocks.reverse)
+    println(freeSpace)
+    println("!")
+    println(take(blocks.reverse, freeSpace).filterNot(_.empty))
+    println(
+      take(blocks.reverse, freeSpace).filterNot(_.empty) == List(
+        Block(2, 9, false),
+        Block(4, 8, false),
+        Block(3, 7, false),
+        Block(3, 6, false)
+      )
+    )
+    println("!")
+
+    process(List.empty, take(blocks.reverse, freeSpace).filterNot(_.empty), blocks)
+
+  def checksum(blocks: List[Block]): BigInt =
     blocks
-      .takeWhile(_ != '.')
+      .takeWhile(!_.empty)
       .zipWithIndex
       .foldLeft(BigInt(0)):
         case (sum, (char, idx)) =>
-          sum + (char.toString.toInt * idx)
+          sum + (char.size * idx)
 
-  override def part1(input: String): BigInt =
-    println(blocks(input))
-    println(defrag(blocks(input)))
-    checksum(defrag(blocks(input)))
+  def render(blocks: List[Block]): String =
+    blocks
+      .flatMap: block =>
+        val ch = if block.empty then "." else block.id
+        List.fill(block.size)(ch)
+      .mkString
+
+  override def part1(input: List[Block]): BigInt =
+    checksum(defrag(input))
--- src/test/scala/aoc/twentyfour/Day9Test.scala
+++ src/test/scala/aoc/twentyfour/Day9Test.scala
@@ -1,20 +1,67 @@
 package aoc.twentyfour
 
+import aoc.twentyfour.Day9.*
+
 class Day9Test extends munit.FunSuite {
   test("blocks") {
-    assertEquals(Day9.blocks("12345"), "0..111....22222")
-    assertEquals(Day9.blocks("2333133121414131402"), "00...111...2...333.44.5555.6666.777.888899")
+    assertEquals(Day9.render(Day9.parse("12345")), "0..111....22222")
+    assertEquals(Day9.render(Day9.parse("2333133121414131402")), "00...111...2...333.44.5555.6666.777.888899")
   }
 
   test("defrag") {
-    assertEquals(Day9.defrag("0..111....22222"), "022111222......")
+//    assertEquals(Day9.render(Day9.defrag(Day9.parse("12345"))), "022111222")
     assertEquals(
-      Day9.defrag("00...111...2...333.44.5555.6666.777.888899"),
-      "0099811188827773336446555566.............."
+      Day9.render(Day9.defrag(Day9.parse("2333133121414131402"))),
+      "0099811188827773336446555566"
     )
   }
 
   test("checksum") {
-    assertEquals(Day9.checksum("0099811188827773336446555566.............."), BigInt(1928))
+    val parsed    = Day9.parse("2333133121414131402")
+    val defragged = Day9.defrag(parsed)
+    assertEquals(Day9.checksum(defragged), BigInt(1928))
+  }
+
+  test("take") {
+    val blocks = List(Block(10, 1, false), Block(20, 1, false), Block(10, 1, false))
+    assertEquals(take(blocks, 11), List(Block(10, 1, false), Block(1, 1, false)))
+    assertEquals(take(blocks, 31), List(Block(10, 1, false), Block(20, 1, false), Block(1, 1, false)))
+    assertEquals(take(blocks, 0), List.empty)
+    assertEquals(take(blocks, 99), blocks)
+
+    assertEquals(
+      take(
+        List(
+          Block(2, 9, false),
+          Block(0, -999, true),
+          Block(4, 8, false),
+          Block(1, -999, true),
+          Block(3, 7, false),
+          Block(1, -999, true),
+          Block(3, 6, false),
+          Block(2, 6, false),
+          Block(1, 6, false)
+        ),
+        14
+      ).filterNot(_.empty),
+      List(
+        Block(2, 9, false),
+        Block(4, 8, false),
+        Block(3, 7, false),
+        Block(3, 6, false)
+      )
+    )
+  }
+
+  test("drop") {
+    val blocks = List(Block(10, 1, false), Block(20, 1, false))
+    assertEquals(drop(blocks, 11), List(Block(19, 1, false)))
+    assertEquals(drop(blocks, 0), blocks)
+    assertEquals(drop(blocks, 99), List.empty)
+  }
+
+  test("pack") {
+    val blocks = List(Block(1, 1, false), Block(1, 1, false), Block(1, 2, false), Block(1, 1, false))
+    assertEquals(pack(blocks), List(Block(2, 1, false), Block(1, 2, false), Block(1, 1, false)))
   }
 }
